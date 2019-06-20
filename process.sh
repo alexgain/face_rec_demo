42eea14277bfce50597cc5657bd731588d8fe374
Alex Gain <doctorjuice1@gmail.com>
2019-06-20 07:47:10 -0400

    minor bugs

--- process.sh
+++ process.sh
@@ -1,4 +1,4 @@
-gdrive download --path 'videos/sean_example.mp4' 1hSprUOwi2fdukhPsAB4oD4x47Kfr5Zxh
+gdrive download --path 'videos' 1hSprUOwi2fdukhPsAB4oD4x47Kfr5Zxh
 wget --no-check-certificate 'https://drive.google.com/file/d/1hSprUOwi2fdukhPsAB4oD4x47Kfr5Zxh' -O 'videos/sean_example.mp4'
 python recognize_faces_video_file.py --encodings encodings.pickle --input videos/sean_example.mp4 --output output/sean_example_output.avi --display 0
 gdrive update 1Cvn8PWopnRdKZQ8UGL_uOkhwF9T8Xj-4 output/sean_example_output.avi
