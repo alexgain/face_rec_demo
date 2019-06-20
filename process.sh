48f9f79d7aa869e6e888b8d903eadb5258169d39
Alex Gain <doctorjuice1@gmail.com>
2019-06-20 06:36:27 -0400

    added sh file

--- process.sh
+++ process.sh
@@ -1,2 +1,2 @@
 python recognize_faces_video_file.py --encodings encodings.pickle --input videos/sean_example.mp4 --output output/sean_example_output.avi --display 0
-gdrive update 1S17ImWj0h9AwDd7ZYq2inFpE8xrBwTTT output/sean_example_output.avi
+gdrive update 1Cvn8PWopnRdKZQ8UGL_uOkhwF9T8Xj-4 output/sean_example_output.avi
